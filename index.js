dbb5b3a9fb85407a7c6fa003f24ba77f4ffee5aa
Simeon Rediger <simeonrediger@gmail.com>
2026-06-13 19:12:44 -0400

    Add index page route

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 import * as http from 'node:http';
+import fs from 'node:fs';
 
-const server = http.createServer();
+const server = http.createServer(route);
 server.listen(8080);
+
+const routes = {
+  '/': 'index.html',
+};
+
+const contentTypes = {
+  html: 'text/html',
+};
+
+function route(request, response) {
+  const filepath = routes[request.url];
+  const extension = filepath.split('.')[1];
+  const contentType = contentTypes[extension];
+
+  if (!filepath || !contentType) {
+    console.error('Bad route:', request.url);
+    return;
+  }
+
+  response.writeHead(200, { 'Content-Type': contentType });
+
+  try {
+    const data = fs.readFileSync(filepath, 'utf8');
+    response.end(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
