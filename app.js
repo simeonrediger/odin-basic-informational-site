986b68cc93facad3da3a2992417e3e2be33cbf0f
Simeon Rediger <simeonrediger@gmail.com>
2026-06-15 12:25:21 -0400

    Capitalize PORT constant

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import * as http from 'node:http';
 
 import route from './route.js';
 
-const port = 8080;
-export const origin = `http://localhost:${port}`;
+const PORT = 8080;
+export const origin = `http://localhost:${PORT}`;
 
 const server = http.createServer(route);
-server.listen(port);
+server.listen(PORT);
